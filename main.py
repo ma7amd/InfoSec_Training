e8000dd5cd8f4addf1771582ba21ba02800dbdf4
Zamba <ma7amd.sala7@gmail.com>
2020-09-13 01:18:18 +0400

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-TCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-TCPSocket.bind(("0.0.0.0", 8000))
-TCPSocket.listen(2)
-(client, (ip, port)) = TCPSocket.accept()
-TCPSocket.close()
